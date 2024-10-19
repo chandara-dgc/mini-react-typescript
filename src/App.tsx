cc40b3bcbca4d82b9c316c2ea88ca0ed65b341dc
chandara-dgc <L.Chandara@dgc.gov.kh>
2024-10-19 13:21:42 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/header";
+import InfoBody from "./components/infoBody";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div id="home-page">
+      <Header />
+      <InfoBody />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
